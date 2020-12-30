86df901148c1b998365b39d993307e0ee0e93949
Mark C. Ocariza <mcocariza@gmail.com>
2020-12-30 16:17:32 +0800

    NEwChanges

---
+++ WebServiceMethod/Model/User_Details.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceMethod.Model
+{
+    public class User_Details
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string civilstatus { get; set; }
+        public int age { get; set; }
+        public DateTime bdate { get; set; }
+        public string religion { get; set; }
+        public string bplace { get; set; }
+
+    }
+}
---
+++ WebServiceMethod/WebServiceMethod/WebServiceBridge.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebServiceBridge.asmx.cs" Class="WebServiceMethod.WebServiceMethod.WebServiceBridge" %>
